3c0c4b210188c3141f8db0393d74d4f6ea9d2134
Roger Moraes <roger.lm.mr@gmail.com>
2020-05-01 15:53:47 -0300

    Database Import Challange. All tests has passed

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+}
+
+class CreateCategoryService {
+  public async execute({ title }: Request): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+
+    let category = await categoryRepository.findOne({
+      where: { title },
+    });
+
+    if (!category) {
+      category = categoryRepository.create({
+        title,
+      });
+      await categoryRepository.save(category);
+    }
+
+    return category;
+  }
+}
+export default CreateCategoryService;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const createCategory = new CreateCategoryService();
+
+    const { total } = await transactionRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Transaction type is invalid');
+    }
+
+    const categoryTransaction = await createCategory.execute({
+      title: category,
+    });
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: categoryTransaction,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
